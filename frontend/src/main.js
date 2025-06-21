a2d45884fb2c6c971bbd9da41b0fdac4f72fdf79
teco-hirasawa <hirasawa.motoki@tecotec.co.jp>
2025-06-21 16:47:34 +0900

    デザイン修正

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,12 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import ja from 'element-plus/es/locale/lang/ja'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: ja,
+})
 
 app.mount('#app')
